43132d623d048a00c59c647b94fa484a25da4675
YY <trans.sector7@gmail.com>
2023-03-27 18:36:54 +0900

    0327

--- CIT_service.py
+++ CIT_service.py
@@ -9,33 +9,33 @@ def madefolder(): #未実装
 	print("madefolder")
 
 def mainloop():
+	while True:
 	# 時刻確認
-	if get_day_of_week() < 5:
-		print("Business day(営業曜日)")
-		print(datetime.today().hour,"hor")
+		if get_day_of_week() < 5:
+			print("Business day(営業曜日)")
+			print(datetime.today().hour,"hor")
 
-		if 11 <= int(datetime.today().hour) < 14:
-			print("Business hours(営業時間)")
-			with ProcessPoolExecutor(max_workers=2) as executor:
-				executor.submit(GET_Narashino1)
-				executor.submit(GET_Narashino2)
-				executor.submit(GET_Tsudanuma)
-			time.sleep(np.random.randint(5,10))
-			mainloop()
+			if 11 <= int(datetime.today().hour) < 14:
+				print("Business hours(営業時間)")
+				with ProcessPoolExecutor(max_workers=2) as executor:
+					executor.submit(GET_Narashino1)
+					executor.submit(GET_Narashino2)
+					executor.submit(GET_Tsudanuma)
+				time.sleep(np.random.randint(5,10))
+
+			else:
+				print("Not in Service(営業時間外)")
+				# 時間外処理を入れる
+				loop_out()
+
 		else:
-			print("Not in Service(営業時間外)")
+			print("Not in Service(曜日外)")
 			# 時間外処理を入れる
 			loop_out()
-
-	else:
-		print("Not in Service(曜日外)")
-		# 時間外処理を入れる
-		loop_out()
 
 def loop_out():
 	print("loop_out")
 	time.sleep(200)
-	mainloop()
 
 def get_day_of_week():
 	print("get_day_of_week-(曜日を取得)")
